d6381c2d6e27e665909353384514f2a072fb159b
unknown <sidduinevitable@gmail.com>
2022-12-27 09:06:16 +0530

    add drag and drop functionality

--- packages/vue-tower-of-hanoi/src/components/TowerOfHanoi.vue
+++ packages/vue-tower-of-hanoi/src/components/TowerOfHanoi.vue
@@ -1,25 +1,92 @@
 <template>
   <div class="horizontal-base">
     <div class="base">
-      <div class="discs">
+      <div class="discs" @drop="onDrop($event, 1)">
         <div
           class="disc"
-          v-for="i in 4"
+          draggable="true"
+          v-for="i in getDiscs(1)"
           :style="{
-            width: 45 * i + 'px',
-            backgroundColor: discColors[i],
+            width: 45 * i.value + 'px',
+            backgroundColor: discColors[i.value],
           }"
+          @dragstart="onDragStart($event, i.id)"
         >
-          {{ i }}
+          {{ i.value }}
         </div>
       </div>
     </div>
-    <div class="base">2</div>
-    <div class="base">3</div>
+    <div
+      class="base"
+      @drop="onDrop($event, 2)"
+      @dragenter.prevent
+      @dragover.prevent
+    >
+      <div class="discs">
+        <div
+          class="disc"
+          draggable="true"
+          v-for="i in getDiscs(2)"
+          :style="{
+            width: 45 * i.value + 'px',
+            backgroundColor: discColors[i.value],
+          }"
+          @dragstart="onDragStart($event, i.id)"
+        >
+          {{ i.value }}
+        </div>
+      </div>
+    </div>
+    <div class="base" @drop="onDrop($event, 3)">
+      <div class="discs">
+        <div
+          class="disc"
+          draggable="true"
+          v-for="i in getDiscs(3)"
+          :style="{
+            width: 45 * i.value + 'px',
+            backgroundColor: discColors[i.value],
+          }"
+          @dragstart="onDragStart($event, i.id)"
+        >
+          {{ i.value }}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { discColors } from "./utils";
+import { ref } from "vue";
+interface Disc {
+  id: number;
+  value: number;
+  list: number;
+}
+const discs = ref<Disc[]>([
+  { id: 1, value: 1, list: 1 },
+  { id: 2, value: 2, list: 1 },
+  { id: 3, value: 3, list: 1 },
+  { id: 4, value: 4, list: 1 },
+]);
+
+const getDiscs = (listIndex: number) => {
+  return discs.value.filter((disc) => disc.list === listIndex);
+};
+const onDragStart = (event: DragEvent, index: number) => {
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("itemId", index.toString());
+  }
+};
+const onDrop = (event: DragEvent, index: number) => {
+  const itemId = event.dataTransfer?.getData("itemId");
+  const disc = discs.value.find((disc) => disc.value.toString() === itemId);
+  if (disc) {
+    disc.list = index;
+  }
+};
 </script>
 <style scoped>
 .horizontal-base {
@@ -27,19 +94,19 @@ import { discColors } from "./utils";
   display: flex;
   justify-content: space-around;
   width: 500px;
-  position: relative;
 }
 .base {
   height: 200px;
   border: 2px solid #ccc;
   padding: 0.5rem;
   border-bottom: none;
+  position: relative;
   background: linear-gradient(to right, #d7b889, #b27315, #966f33);
 }
 .discs {
   position: absolute;
   bottom: 0;
-  left: -10px;
+  left: -85px;
   display: flex;
   flex-direction: column;
   align-items: center;

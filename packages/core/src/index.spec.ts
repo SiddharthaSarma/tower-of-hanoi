bdbddf6336da8d2ab190fefa943065fa1bfe9b7a
unknown <sidduinevitable@gmail.com>
2022-12-29 16:25:50 +0530

    format the code

--- packages/core/src/index.spec.ts
+++ packages/core/src/index.spec.ts
@@ -16,9 +16,23 @@ describe('core', () => {
   it('should change position when we try to move discs', () => {
     const discs = [[...getInitialDisc(3)], [], []];
     let result = updateDiscPosition(discs, 1, 3, 1);
-    expect(result).toEqual([[{id: 2, value: 2}, {id: 3, value: 3}], [], [{id: 1, value: 1}]]);
+    expect(result).toEqual([
+      [
+        { id: 2, value: 2 },
+        { id: 3, value: 3 },
+      ],
+      [],
+      [{ id: 1, value: 1 }],
+    ]);
     result = updateDiscPosition(result, 1, 2, 2);
     result = updateDiscPosition(result, 3, 2, 1);
-    expect(result).toEqual([[{id: 3, value: 3}], [{id: 1, value: 1}, {id: 2, value: 2}], []]);
+    expect(result).toEqual([
+      [{ id: 3, value: 3 }],
+      [
+        { id: 1, value: 1 },
+        { id: 2, value: 2 },
+      ],
+      [],
+    ]);
   });
 });

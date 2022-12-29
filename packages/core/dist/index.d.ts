96b8574ef03fbac3dc2263b666fd3a78d6e73b4c
unknown <sidduinevitable@gmail.com>
2022-12-29 06:00:28 +0530

    remove dist

--- packages/core/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-interface Disc {
-    id: number;
-    value: number;
-}
-declare const getInitialDisc: (count: number) => Disc[];
-declare const updateDiscPosition: (discs: Disc[][], from: number, to: number, discNo: number) => Disc[][];
-
-export { Disc, getInitialDisc, updateDiscPosition };
